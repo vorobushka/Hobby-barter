64f21369741f0154c674ed7aeda7651dc3926092
admin <1vorobushka1@gmail.com>
2019-10-31 15:17:23 +0300

    alert

---
+++ front/my-app/src/components/Alert/Alert.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Alert extends Component {
+ 
+  render() {
+    return (
+<div class="alert alert-info" role="alert">
+  <strong>Heads up!</strong> Congratulations!!!
+</div>
+      
+    );
+  }
+}
---
+++ front/my-app/src/components/Alert/index.js
@@ -0,0 +1,3 @@
+import Alert from './App'
+
+export default Alert
