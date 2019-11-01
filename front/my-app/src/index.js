cb7e45d85dad5abc17d2aba8b942f2d38f39fb7d
Alekskupr <leshakuprish@yandex.ru>
2019-11-01 18:32:30 +0300

    redux profile

--- front/my-app/src/index.js
+++ front/my-app/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './redux/reducer';
 
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
