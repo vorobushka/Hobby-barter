cca8778efc0df5cac7e97e34056f5365593b63f8
admin <1vorobushka1@gmail.com>
2019-11-07 16:51:42 +0300

    design login

--- front/my-app/src/components/login/login.js
+++ front/my-app/src/components/login/login.js
@@ -2,6 +2,7 @@ import './login.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { profileUserAC } from '../../redux/actions';
+import { Link } from 'react-router-dom';
 
 class Login extends Component {
   constructor(props) {
@@ -42,29 +43,92 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="div-form">
-        <form className="form-" onSubmit={this.login}>
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              name="login"
-              placeholder="введите логин"
-              onChange={this.loginInStateLogin}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              placeholder="введите пароль"
-              onChange={this.passwordInStateLogin}
-            />
-          </div>
-          <button type="submit" className="btn btn-light">Отправить</button>
-        </form>
-      </div>
+
+
+      <div className="container div-form">
+        <div className="d-flex justify-content-center h-100">
+          <div className="card">
+            <div className="card-header">
+              <h3>Войти</h3>
+              <div className="d-flex justify-content-end social_icon">
+                <span><i className="fab fa-facebook-square"></i></span>
+                <span><i className="fab fa-google-plus-square"></i></span>
+                <span><i className="fab fa-twitter-square"></i></span>
+              </div>
+            </div>
+            <div className="card-body">
+              <form onSubmit={this.login}>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text"><i className="fas fa-user"></i></span>
+                  </div>
+                  <input type="text" className="form-control" placeholder="введите логин" onChange={this.loginInStateLogin}/>
+						
+					</div>
+                  <div className="input-group form-group">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text"><i className="fas fa-key"></i></span>
+                    </div>
+                  <input type="password" className="form-control" placeholder="введите пароль" onChange={this.passwordInStateLogin}/>
+					</div>
+                    <div className="row align-items-center remember">
+                      <input type="checkbox"/>Запомнить
+					</div>
+                      <div className="form-group">
+                  <button type="submit" value="Войти" className="btn float-right btn-light">Отправить</button>
+					</div>
+				</form>
+                    </div>
+                    <div className="card-footer">
+                      <div className="d-flex justify-content-center links">
+                У вас нет аккаунта?<Link to="/registration">Зарегистрироваться</Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      // <div className="div-form">
+      //   <form className="form-" onSubmit={this.login}>
+      //     <div className="form-group">
+      //       <input
+      //         type="text"
+      //         className="form-control"
+      //         name="login"
+      //         placeholder="введите логин"
+      //         onChange={this.loginInStateLogin}
+      //       />
+      //     </div>
+      //     <div className="form-group">
+      //       <input
+      //         type="password"
+      //         name="password"
+      //         className="form-control"
+      //         placeholder="введите пароль"
+      //         onChange={this.passwordInStateLogin}
+      //       />
+      //     </div>
+      //     <button type="submit" className="btn btn-light">Отправить</button>
+      //   </form>
+      // </div>
     );
   }
 }
